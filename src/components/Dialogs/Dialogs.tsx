1df5b4e12660e5587092b248ebbfd64599858784
Samuil <samuil-18@tut.by>
2022-11-24 01:47:16 +0100

    change data in Dialogs.tsx, MyPosts.tsx and Profile.tsx

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,45 +2,72 @@ import React from "react";
 import s from "./Dialogs.module.css"
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
+import {v1} from "uuid";
 
 
 export function Dialogs() {
+
+    const dialogItemData = [
+        {
+            name: "Sam",
+            id: v1(),
+            userAvatar: "https://tiny.cc/public/images/robot_small.png",
+
+        },
+        {
+            name: "Alena",
+            id: v1(),
+            userAvatar: "https://shorturlbot.com/wp-content/uploads/2020/05/faq.png",
+
+        },
+        {
+            name: "Cat",
+            id: v1(),
+            userAvatar: "https://global-uploads.webflow.com/5f68a65d0932e3546d41cc61/5f9abc8e946c86c2ccc0e6f6_1603976334184-short-url-link-shortener-best-shopify-apps.png",
+        },
+
+    ]
+
+    const messagesData = [
+        {
+            id: v1(),
+            message: "Hello, men!",
+        },
+        {
+            id: v1(),
+            message: "How are You?",
+        },
+        {
+            id: v1(),
+            message: "Bla...",
+        },
+        {
+            id: v1(),
+            message: "Boring",
+        },
+        {
+            id: v1(),
+            message: "Hey, are you here?",
+        },
+    ]
+
     return (
 
         <div className={s.dialogs}>
 
             <div className={s.dialogItem}>
-                <DialogItem
-                    name={"Sam"}
-                    id={"1"}
-                    userAvatar={"https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/283462437_3171250703150174_7038408124234808761_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=smpcAP4Lj_oAX8uCyI4&tn=ZZdWJvwQXACe1_gF&_nc_ht=scontent-waw1-1.xx&oh=00_AfAZ2H8medh18wgbVsHd-Sg94W1UMZ7d8mhBaRXBuq_Ucg&oe=637CD0C1"}
-                />
-                <DialogItem
-                    name={"Alena"}
-                    id={"2"}
-                    userAvatar={"https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/279497715_1174226816711339_3401783792482775809_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=JXLsODScaZ4AX87ZVSa&_nc_ht=scontent-waw1-1.xx&oh=00_AfCtAYNcamWo9TUKuuh5V-9ioS-IgS2NVW7fsJYMPZgAYg&oe=637E340E"}
-                />
-                <DialogItem
-                    name={"Cat"}
-                    id={"3"}
-                    userAvatar={"https://thumbs.img-sprzedajemy.pl/1000x901c/71/c2/91/piekna-kotka-bengalska-bengalski-bielsko-biala-525715748.jpg"}
-                />
-                <DialogItem
-                    name={"Eat"}
-                    id={"4"}
-                    userAvatar={"https://cdn.dribbble.com/users/5269228/screenshots/17512397/media/3bec42f2a11d9ec2b4a2137caf54cc8f.png?compress=1&resize=400x300&vertical=top"}
-                />
+                {dialogItemData.map(i => {
+                        return <DialogItem name={i.name} id={i.id} userAvatar={i.userAvatar}/>
+                    })}
 
 
             </div>
 
             <div className={s.messages}>
-
-                <Message message={"Hello, men!"}/>
-                <Message message={"How are You?"}/>
-                <Message message={"Bla..."}/>
-                <Message message={"Boring"}/>
-                <Message message={"Hey, are you here?"}/>
+                {
+                    messagesData.map(i => {
+                        return <Message message={i.message}/>
+                    })}
 
 
             </div>
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,20 +1,41 @@
 import s from "./MyPosts.module.css";
 import React from "react";
 import Post from "./Post/Post";
+import { v1 } from "uuid";
 
 
 const MyPosts = () => {
+
+    const postsData = [
+        {
+            id: v1(),
+            message: "Hello, Props!",
+            likes: "23",
+        },
+        {
+            id: v1(),
+            message: "Wow, its cool!",
+            likes: "28",
+        },
+
+
+    ]
+
     return (
         <div className={s.posts}>
             <h2>MY POSTS</h2>
             <div className={s.item}>
-               <h4>New Post</h4>
-                <textarea placeholder={"What new?"} ><input type="text" /></textarea>
-
+                <h4>New Post</h4>
+                <textarea placeholder={"What new?"}><input type="text"/></textarea>
                 <button className={s.button}>Published</button>
             </div>
-            <Post message = 'Hello, Props!' likes = '23'/>
-            <Post message = 'Wow, its cool!' likes = '28'/>
+
+            {
+                postsData.map(i => {
+                   return  <Post message={i.message} likes={i.likes}/>
+                })}
+
+
         </div>
     )
 }
